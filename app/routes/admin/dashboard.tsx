c5be88ccc61734027f0237e821d5e341aeebaaa6
aleehjoo <alejandroumila2@gmail.com>
2025-07-18 16:29:50 +0800

    Primary mobile and desktop dashboard layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Header} from "../../../components";
 
 const Dashboard = () => {
+    const user = { name: 'Alejandro'}
     return (
-        <div>Dashboard</div>
+        <main className="dashboard wrapper">
+            <Header
+                title={`Welcome ${user?.name ?? 'Guest'} 👋`}
+                description={"Track activity, trends and popular destinations in real time"}
+            />
+
+            Dashboard Page Contents
+        </main>
     )
 }
 export default Dashboard
