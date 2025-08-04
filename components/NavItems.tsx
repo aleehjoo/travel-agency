e89e5c3025cb6069b7e111ec0360060f05b7766b
aleehjoo <alejandroumila2@gmail.com>
2025-08-04 17:06:25 +0800

    Implemented logout and additional auth functionalities

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,17 @@
-import {Link, NavLink} from "react-router";
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router";
 import {sidebarItems} from "~/constants";
 import {cn} from "~/lib/utils";
+import {logoutUser} from "~/appwrite/auth";
 
-const NavItems = ({ handleClick }: {handleClick: () => void}) => {
-    const user = {
-        name: 'Name here',
-        email: 'placeholder@example.com',
-        image: '/assets/images/david.webp'
+const NavItems = ({ handleClick }: {handleClick?: () => void}) => {
+    const user = useLoaderData();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in');
     }
+
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
@@ -37,14 +41,14 @@ const NavItems = ({ handleClick }: {handleClick: () => void}) => {
                 </nav>
 
                 <footer className='nav-footer'>
-                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'}/>
+                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} referrerPolicy='no-referrer'/>
                     <article>
                         <h2>{user?.name}</h2>
                         <p>{user?.email}</p>
                     </article>
 
                     <button
-                    onClick={() => {console.log('logout')}}
+                    onClick={handleLogout}
                     className='cursor-pointer'>
                         <img src='/assets/icons/logout.svg' alt='Logout' className='size-6'/>
                     </button>
