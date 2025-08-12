072372362d27b83b06468397d134333f3f721a08
aleehjoo <alejandroumila2@gmail.com>
2025-08-12 21:16:42 +0800

    Added info pills in trip details

--- app/routes/admin/trip-detail.tsx
+++ app/routes/admin/trip-detail.tsx
@@ -2,26 +2,48 @@ import type {LoaderFunctionArgs} from "react-router";
 import {getTripById} from "~/appwrite/trips";
 import type { Route } from './+types/trip-detail';
 import {parseTripData} from "~/lib/utils";
-import {Header} from "../../../components";
+import {Header, InfoPill} from "../../../components";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
     const { tripId } = params;
 
     if(!tripId) throw new Error('Trip ID is required');
 
-    const trip = await getTripById(tripId);
-
-    return trip;
+    return await getTripById(tripId);
 }
 
 const TripDetail = ({ loaderData }: Route.ComponentProps) => {
-    const tripData = parseTripData(loaderData?.trip);
+    console.log(loaderData)
+    const tripData = parseTripData(loaderData?.tripDetail);
 
-    const { name } = tripData || {};
+    const { name, duration, itinerary, travelStyle, groupType, budget, interests,
+        estimatedPrice, description, bestTimeToVisit, weatherInfo, country} = tripData || {};
 
     return (
         <main className="trip-detail wrapper">
-            <Header title="Trip Detail" description={`View and edit ${name} trip`} />
+            <Header title="Trip Details" description="View and edit AI-generated travel plans" />
+
+            <section className="container wrapper-md">
+                <header>
+                    <h1 className="p-40-semibold text-dark-100">{name}</h1>
+
+                    <div className="flex items-center gap-5">
+                        <InfoPill
+                            text={`${duration} day plan`}
+                            image="/assets/icons/calendar.svg"
+                        />
+
+                        <InfoPill
+                            text={itinerary?.slice(0,2).map((item) => item.location).join(', ')||''}
+                            image="/assets/icons/location-mark.svg"
+                        />
+                    </div>
+                </header>
+                <section className="gallery">
+
+                </section>
+
+            </section>
 
         </main>
     )
